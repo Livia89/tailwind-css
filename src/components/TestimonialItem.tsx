8fb76ffc440f772629de120396bdb97c54c9b717
Livia89 <liviapriscilla1989@gmail.com>
2022-05-11 12:40:32 +0100

    Adding files to repo

---
+++ src/components/TestimonialItem.tsx
@@ -0,0 +1,23 @@
+export const TestimotialItem = () => {
+  return (
+    <div className="bg-gray-100 p-8 shadow-sm rounded-lg md:flex md:p-1">
+      <img
+        className="rounded-full mx-auto md:rounded-none "
+        src="https://static.vecteezy.com/ti/vetor-gratis/t2/1993889-icone-personagem-avatar-mulher-latina-bonita-gr%C3%A1tis-vetor.jpg"
+        alt=""
+      />
+      <div className="pt-6 text-center space-y-4 md:text-left md:pl-8">
+        <div className="text-md font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem libero
+          possimus tempora laudantium provident, atque ex minima voluptatum
+          doloremque vel. Eum repudiandae repellat et provident minus incidunt
+          nam fuga sequi.
+        </div>
+        <div className="font-medium">
+          <div className="text-blue-600 font-bold">Maria José</div>
+          <div className="text-gray-500">Aluna</div>
+        </div>
+      </div>
+    </div>
+  );
+};
